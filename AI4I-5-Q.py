5ee19dc43646333bb6007c7a99da4d24496e90b6
Z Taeminnie <z.taeminnie@gmail.com>
2020-05-30 17:00:10 +0800

    AISG Quiz 5

--- AI4I-5-Q.py
+++ AI4I-5-Q.py
@@ -1,15 +1,32 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_absolute_error
+# from sklearn.metrics import mean_squared_error
 
 
 # Seen Data (used for training)
 # Unseen Data (unavailable for training)
 
 train = pd.read_csv("train.csv")
-print(len(train))
+# print(len(train))
+# print(train.info())
+print('Median value for SalePrice: '+ str(np.median(train['SalePrice'])))
+print('Unique Values in SaleType: '+ str(len(train.SaleType.unique())))
+
+# plt.hist(train.SalePrice, bins = 30)
+# plt.show()
+
+print('Correlation between 1stFlrSF and SalePrice: ' + str(train['1stFlrSF'].corr(train['SalePrice'])))
+print('Correlation between YearBuilt and SalePrice: ' + str(train['YearBuilt'].corr(train['SalePrice'])))
+print('Correlation between OverallQual and SalePrice: ' + str(train['OverallQual'].corr(train['SalePrice'])))
+print('Correlation between BsmtFinSF1 and SalePrice: ' + str(train['BsmtFinSF1'].corr(train['SalePrice'])))
+
+print('Missing value for PoolQC : '+ str((train.PoolQC.isnull().sum())))
+print('Missing value for MiscFeature  : '+ str((train.MiscFeature .isnull().sum())))
+print('Missing value for LotFrontage : '+ str((train.LotFrontage.isnull().sum())))
+
 # X = pd.get_dummies(train.iloc[:,0:9])
 # y = train.iloc[:, 9]
 
