b49d0ad5babfc33be63b04271b85ddc0c3b14800
zoiet <z.taeminnie@gmail.com>
2020-06-03 18:23:01 +0800

    Cluster Analysis in Python

---
+++ Cluster Analysis in Python.py
@@ -0,0 +1,388 @@
+###################################################################################
+# Unsupervised learning: basic
+Labeled and unlabeled data
+
+Unsupervised learning => a group of machine learning algorithms that find patterns in data
+    - Data for algorithms has not been labeled, classified or characterized
+    - The objective of the algorithm is to interpret any structrure in the data
+    - Common unsupervised learning algorithms: clustering, neural networks, anomaly detection
+
+Clustering
+    + The process of grouping items with similar characteristic
+
+
+###################################################################################
+# Pokémon sightings
+# Import plotting class from matplotlib library
+from matplotlib import pyplot as plt
+
+# Create a scatter plot
+plt.scatter(x, y)
+
+# Display the scatter plot
+plt.show()
+
+###################################################################################
+# Pokémon sightings: hierarchical clustering
+# Import linkage and fcluster functions
+from scipy.cluster.hierarchy import linkage, fcluster
+
+# Use the linkage() function to compute distance
+Z = linkage(df, 'ward')
+
+# Generate cluster labels
+df['cluster_labels'] = fcluster(Z, 2,criterion='maxclust')
+
+# Plot the points with seaborn
+sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
+plt.show()
+###################################################################################
+# Pokémon sightings: k-means clustering
+# Import kmeans and vq functions
+from scipy.cluster.vq import kmeans, vq
+
+# Compute cluster centers
+centroids,_ = kmeans(df, 2)
+
+# Assign cluster labels
+df['cluster_labels'], _ = vq(df, centroids)
+
+# Plot the points with seaborn
+sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
+plt.show()
+###################################################################################
+# Normalize basic list data
+# Import the whiten function
+from scipy.cluster.vq import whiten
+
+goals_for = [4,3,2,3,1,1,2,0,1,4]
+
+# Use the whiten() function to standardize the data
+scaled_data = whiten(goals_for)
+print(scaled_data)
+###################################################################################
+# Visualize normalized data
+# Plot original data
+plt.plot(goals_for, label='original')
+
+# Plot scaled data
+plt.plot(scaled_data, label='scaled')
+
+# Show the legend in the plot
+plt.legend()
+
+# Display the plot
+plt.show()
+###################################################################################
+# Normalization of small numbers
+# Prepare data
+rate_cuts = [0.0025, 0.001, -0.0005, -0.001, -0.0005, 0.0025, -0.001, -0.0015, -0.001, 0.0005]
+
+# Use the whiten() function to standardize the data
+scaled_data = whiten(rate_cuts)
+
+# Plot original data
+plt.plot(rate_cuts, label='original')
+
+# Plot scaled data
+plt.plot(scaled_data, label='scaled')
+
+plt.legend()
+plt.show()
+###################################################################################
+# FIFA 18: Normalize data
+# Scale wage and value
+fifa['scaled_wage'] = whiten(fifa['eur_wage'])
+fifa['scaled_value'] = whiten(fifa['eur_value'])
+
+# Plot the two columns in a scatter plot
+fifa.plot(x='scaled_wage', y='scaled_value', kind = 'scatter')
+plt.show()
+
+# Check mean and standard deviation of scaled values
+print(fifa[['scaled_wage', 'scaled_value']].describe())
+###################################################################################
+# Basics of hierarchical clustering
+Creating a distance matrix using linkage
+scripy.cluster.hierarchy.linkage(observations, method='single', metric='euclidean', optimal_ordering=False)
+
+single: based on 2 cloest objects
+complete: based on two farthest objectsaverage: based on the arithmetic mean of all objects
+centroid: based on the geometric mean of all objects
+median: based on the median of all objects
+ward: based on the sum of squares
+
+Creating cluster labels with fcluster
+scripy.cluster.hierarchy.fcluster(distance_matrix, num_clusters, criterion)
+distance_matrix: output of linkage() method
+num_clusters: number of cluster_labels
+criterion: how to decide thresholds to form clusters
+
+
+###################################################################################
+# Hierarchical clustering: ward method
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method='ward', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = comic_con)
+plt.show()
+###################################################################################
+# Hierarchical clustering: single method
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method='single', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = comic_con)
+plt.show()
+###################################################################################
+# Hierarchical clustering: complete method
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method='complete', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = comic_con)
+plt.show()
+###################################################################################
+# Visualize clusters with matplotlib
+# Import the pyplot class
+from matplotlib import pyplot as plt
+
+# Define a colors dictionary for clusters
+colors = {1:'red', 2:'blue'}
+
+# Plot a scatter plot
+comic_con.plot.scatter(x='x_scaled', 
+                	   y='y_scaled',
+                       c=comic_con['cluster_labels'].apply(lambda x: colors[x]))
+plt.show()
+###################################################################################
+# Visualize clusters with seaborn
+# Import the seaborn module
+import seaborn as sns
+
+# Plot a scatter plot using seaborn
+sns.scatterplot(x='x_scaled', 
+                y='y_scaled', 
+                hue='cluster_labels', 
+                data = comic_con)
+plt.show()
+###################################################################################
+# Create a dendrogram
+# Import the dendrogram function
+from scipy.cluster.hierarchy import dendrogram
+
+# Create a dendrogram
+dn = dendrogram(distance_matrix)
+
+# Display the dendogram
+plt.show()
+###################################################################################
+# Use of timeit module
+
+from scripy.cluster.hi import linkage
+import pandas as pd 
+import random, timeit 
+points = 100
+df = pd.DataFrame({'x': random.sample(range(0,points), points),
+'y': random.sample(range(0, points), points)}) timeit linkage(df[['x', 'y']], method = 'ward', metric = 'euclidean')})
+
+Comparison of runtime of linkage method
+    - increaing runtime with data points
+    - quadratic increase of runtime
+    - hierarchical method not feasible for large datasets
+###################################################################################
+# FIFA 18: exploring defenders
+# Fit the data into a hierarchical clustering algorithm
+distance_matrix = linkage(fifa[['scaled_sliding_tackle', 'scaled_aggression']], 'ward')
+
+# Assign cluster labels to each row of data
+fifa['cluster_labels'] = fcluster(distance_matrix, 3, criterion='maxclust')
+
+# Display cluster centers of each cluster
+print(fifa[['scaled_sliding_tackle', 'scaled_aggression', 'cluster_labels']].groupby('cluster_labels').mean())
+
+# Create a scatter plot through seaborn
+sns.scatterplot(x='scaled_sliding_tackle', y='scaled_aggression', hue='cluster_labels', data=fifa)
+plt.show()
+###################################################################################
+# K-means clustering: first exercise
+# Import the kmeans and vq functions
+from scipy.cluster.vq import kmeans, vq
+
+# Generate cluster centers
+cluster_centers, distortion = kmeans(comic_con[['x_scaled', 'y_scaled']], 2)
+
+# Assign cluster labels
+comic_con['cluster_labels'], distortion_list = vq(comic_con[['x_scaled', 'y_scaled']], cluster_centers)
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = comic_con)
+plt.show()
+###################################################################################
+# Impact of seeds on distinct clusters
+# Import random class
+from numpy import random
+
+# Initialize seed
+random.seed([1, 2, 1000])
+
+# Run kmeans clustering
+cluster_centers, distortion = kmeans(comic_con[['x_scaled', 'y_scaled']], 2)
+comic_con['cluster_labels'], distortion_list = vq(comic_con[['x_scaled', 'y_scaled']], cluster_centers)
+
+# Plot the scatterplot
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = comic_con)
+plt.show()
+###################################################################################
+# Uniform clustering patterns
+# Import the kmeans and vq functions
+from scipy.cluster.vq import kmeans, vq
+
+# Generate cluster centers
+cluster_centers, distortion = kmeans(mouse[['x_scaled', 'y_scaled']], 3)
+
+# Assign cluster labels
+mouse['cluster_labels'], distortion_list = vq(mouse[['x_scaled', 'y_scaled']], cluster_centers)
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = mouse)
+plt.show()
+###################################################################################
+# FIFA 18: defenders revisited
+# Set up a random seed in numpy
+random.seed([1000,2000])
+
+# Fit the data into a k-means algorithm
+cluster_centers,_ = kmeans(fifa[['scaled_def', 'scaled_phy']], 3)
+
+# Assign cluster labels
+fifa['cluster_labels'], _ = vq(fifa[['scaled_def', 'scaled_phy']], cluster_centers)
+
+# Display cluster centers 
+print(fifa[['scaled_def', 'scaled_phy', 'cluster_labels']].groupby('cluster_labels').mean())
+
+# Create a scatter plot through seaborn
+sns.scatterplot(x='scaled_def', y='scaled_phy', hue='cluster_labels', data=fifa)
+plt.show()
+###################################################################################
+# Extract RGB values from image
+# Import image class of matplotlib
+from matplotlib import image as img
+
+# Read batman image and print dimensions
+batman_image = img.imread('batman.jpg')
+print(batman_image.shape)
+
+# Store RGB values of all pixels in lists r, g and b
+for i in batman_image:
+    for temp_r, temp_g, temp_b in i:
+        r.append(temp_r)
+        g.append(temp_g)
+        b.append(temp_b)
+###################################################################################
+# How many dominant colors?
+distortions = []
+num_clusters = range(1, 7)
+
+# Create a list of distortions from the kmeans function
+for i in num_clusters:
+    cluster_centers, distortion = kmeans(batman_df[['scaled_red', 'scaled_blue', 'scaled_green']], i)
+    distortions.append(distortion)
+
+# Create a data frame with two lists, num_clusters and distortions
+elbow_plot = pd.DataFrame({'num_clusters': num_clusters, 'distortions': distortions})
+
+# Create a line plot of num_clusters and distortions
+sns.lineplot(x='num_clusters', y='distortions', data = elbow_plot)
+plt.xticks(num_clusters)
+plt.show()
+###################################################################################
+# Display dominant colors
+# Get standard deviations of each color
+r_std, g_std, b_std = batman_df[['red', 'green', 'blue']].std()
+
+for cluster_center in cluster_centers:
+    scaled_r, scaled_g, scaled_b = cluster_center
+    # Convert each standardized value to scaled value
+    colors.append((
+        scaled_r * r_std / 255,
+        scaled_g * g_std / 255,
+        scaled_b * b_std / 255
+    ))
+
+# Display colors of cluster centers
+plt.imshow([colors])
+plt.show()
+###################################################################################
+# TF-IDF of movie plots
+# Import TfidfVectorizer class from sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Initialize TfidfVectorizer
+tfidf_vectorizer = TfidfVectorizer(min_df=0.1, max_df=0.75, max_features=50, tokenizer=remove_noise)
+
+# Use the .fit_transform() method on the list plots
+tfidf_matrix = tfidf_vectorizer.fit_transform(plots)
+###################################################################################
+# Top terms in movie clusters
+num_clusters = 2
+
+# Generate cluster centers through the kmeans function
+cluster_centers, distortion = kmeans(tfidf_matrix.todense(), num_clusters)
+
+# Generate terms from the tfidf_vectorizer object
+terms = tfidf_vectorizer.get_feature_names()
+
+for i in range(num_clusters):
+    # Sort the terms and print top 3 terms
+    center_terms = dict(zip(terms, list(cluster_centers[i])))
+    sorted_terms = sorted(center_terms, key=center_terms.get, reverse=True)
+    print(sorted_terms[:3])
+###################################################################################
+# Basic checks on clusters
+# Print the size of the clusters
+print(fifa.groupby('cluster_labels')['ID'].count())
+
+# Print the mean value of wages in each cluster
+print(fifa.groupby('cluster_labels')['eur_wage'].mean())
+###################################################################################
+# FIFA 18: what makes a complete player?
+# Create centroids with kmeans for 2 clusters
+cluster_centers,_ = kmeans(fifa[scaled_features], 2)
+
+# Assign cluster labels and print cluster centers
+fifa['cluster_labels'], _ = vq(fifa[scaled_features], cluster_centers)
+print(fifa.groupby('cluster_labels')[scaled_features].mean())
+
+# Plot cluster centers to visualize clusters
+fifa.groupby('cluster_labels')[scaled_features].mean().plot(legend=True, kind='bar')
+plt.show()
+
+# Get the name column of first 5 players in each cluster
+for cluster in fifa['cluster_labels'].unique():
+    print(cluster, fifa[fifa['cluster_labels'] == cluster]['name'].values[:5])
+###################################################################################
